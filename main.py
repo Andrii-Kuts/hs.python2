abf1f1114ff46264de7326a96e8d681ee050af48
Andrii-Kuts-Work <andrii.kuts.work@gmail.com>
2025-11-19 17:26:40 +0100

    Removed deprecated methods

--- main.py
+++ main.py
@@ -9,26 +9,6 @@ import plotter
 def migrate_to_db():
     ...
 
-# Generates analytics based on the data
-def generate_analystics():
-    ...
-
-# Shows current users leaderboard
-def leaderboard():
-    ...
-
-# Shows individual user analytics
-def user_analytics(used):
-    ...
-
-# Shows timeline of best users
-def timeline():
-    ...
-
-# Shows global analytics, like the total number of interactions
-def global_statistics():
-    ...
-
 def main():
     messenger.notify_app_started()
     read_options()

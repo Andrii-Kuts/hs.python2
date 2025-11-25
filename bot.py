db2588605802b1215a14108bf6bbd8af14bf2468
Andrii-Kuts-Work <andrii.kuts.work@gmail.com>
2025-11-26 00:13:36 +0100

    Improved bot text design

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from database import Database
 import uuid
 from analytics import Analytics
 from plotter import PlotterPool
-from asyncio import get_event_loop
+from asyncio import CancelledError, get_event_loop, sleep, create_task
 
 class PesunBot:
     def __init__(self):
@@ -16,7 +16,7 @@ class PesunBot:
         self.app = ApplicationBuilder().token(os.getenv("TELEGRAM_BOT_KEY")).post_init(self.init_bot).build()
 
         self.app.add_handler(CommandHandler("import", self.handle_import, filters.ALL))
-        self.app.add_handler(CommandHandler("statistics", self.handle_statistics, filters.ALL))
+        self.app.add_handler(CommandHandler("analytics", self.handle_analytics, filters.ALL))
         self.app.add_handler(MessageHandler(filters.ALL, self.handle_import_file))
 
     def run(self):
@@ -27,7 +27,7 @@ class PesunBot:
         bot: telegram.Bot = app.bot
         commands = [
             telegram.BotCommand("/import", "Send chat history as a zip archive to import entire pesun data"),
-            telegram.BotCommand("/statistics", "Builds an app with statistics and sends a link to it"),
+            telegram.BotCommand("/analytics", "Builds an app with analytics and sends a link to it"),
         ]
         await bot.set_my_commands(commands)
 
@@ -49,14 +49,28 @@ class PesunBot:
         file_path = f"cache/downloads/{name}"
         os.makedirs("cache/downloads", exist_ok=True)
         file = await telegram_file.download_to_drive(file_path)
-        response = await response.edit_text(f"⏱️  Parsing...")
+
+        def get_progress_blocks_cnt(percent: float):
+            cnt = 10
+            return int(percent / cnt)
+        def get_progress_text(percent: float):
+            cnt = 10
+            blocks = get_progress_blocks_cnt(percent)
+            number = blocks * cnt
+            return str(number) + "%  " + ("🟦" * blocks)
+        response = await response.edit_text(f"⏱️  Parsing {get_progress_text(0)}")
+        parsing_progress_cnt = get_progress_blocks_cnt(0)
         dataset = None
         async for parse_result in parse_zip(file):
             if parse_result.isDone():
                 dataset = parse_result.dataset
                 break
             else:
-                response = await response.edit_text(f"⏱️  Parsing file {parse_result.file_number}/{parse_result.total_files}")
+                percent = parse_result.file_number/parse_result.total_files * 100
+                new_blocks_cnt = get_progress_blocks_cnt(percent)
+                if parsing_progress_cnt != new_blocks_cnt:
+                    response = await response.edit_text(f"⏱️  Parsing {get_progress_text(percent)}")
+                    parsing_progress_cnt = new_blocks_cnt
         if dataset is None:
             response = await response.edit_text("❌  Could not parse the file. Are you sure its a correct zip archive?")
             return
@@ -71,7 +85,7 @@ class PesunBot:
         self.import_users.add((update.effective_user.id, update.effective_chat.id))
         await update.message.reply_text(f"Now send the zip archive")
 
-    async def handle_statistics(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    async def handle_analytics(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         response = await update.message.reply_text(f"⏱️  Reading the dataset...")
         db = await Database.get_instance()
         dataset = await db.read_dataset(update.effective_chat.id)
@@ -82,7 +96,7 @@ class PesunBot:
         analytics = Analytics(dataset)
         response = await response.edit_text(f"⏱️  Starting dash app...")
         plotterData = await PlotterPool.get_instance().get_plotter(update.effective_chat.id, analytics)
-        response = await response.edit_text(f"✅  View statistics here:\n\nhttp://0.0.0.0:8050{plotterData.path}")
+        response = await response.edit_text(f"📊  Link to analytics:\n\nhttp://0.0.0.0:8050{plotterData.path}")
 
     async def stop_async(self):
         await PlotterPool.get_instance().stop_plotters()
